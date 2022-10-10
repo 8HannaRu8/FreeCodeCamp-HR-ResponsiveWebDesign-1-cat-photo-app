2656c52b52f84b7da6c4612b6dfb262e15ca876a
Hanna Ru <112951096+8HannaRu8@users.noreply.github.com>
2022-10-10 16:44:17 +0300

    Update main.js

--- JavaScript-Algorithms-Data-Structures/BasicJS/main.js
+++ JavaScript-Algorithms-Data-Structures/BasicJS/main.js
@@ -1,1 +1,8 @@
+Before
+var “myStr” = "<a href=\"https://developer.org\" target=\"_blank\">link</a>";
+After!
+var “myStr” = '<a href="https://developer.org" target="_blank">link</a>';
 
+Use Backticks `(it is under 'Esk' key)
+var “myStr” = `'<a href="https://developer.org" target="_blank">link</a>'`;
+with Backsticks u can use ' and " inside the code
