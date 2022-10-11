8aaa3f42e32821d765c903584cfea51bb4ccd5c2
Hanna Ru <112951096+8HannaRu8@users.noreply.github.com>
2022-10-11 19:00:15 +0300

    Create main4.js

---
+++ JavaScript-Algorithms-Data-Structures/BasicJS/main4.js
@@ -0,0 +1,7 @@
+function randomRange(ourMin, ourMax) {
+return Math.floor(Math.random() * (ourMax - ourMin + 1))+ ourMin;
+}
+randomRange(1,9);
+
+var ourRandom = randomRange(5,15);
+console.log(ourRandom);
