e789fe4f6f4885a8cfeeca765283e9a4be736cbe
Hanna Ru <112951096+8HannaRu8@users.noreply.github.com>
2022-10-13 10:28:46 +0300

    Update main4.js

--- JavaScript-Algorithms-Data-Structures/BasicJS/main4.js
+++ JavaScript-Algorithms-Data-Structures/BasicJS/main4.js
@@ -5,3 +5,23 @@ randomRange(1,9);
 
 var ourRandom = randomRange(5,15);
 console.log(ourRandom);
+
+
+
+function checkEqual(a,b) {
+if(a===b) {
+return true;
+}
+else {
+return false;
+}
+}
+console.log(checkEqual(1,2));
+
+
+
+Turnary operator:
+function checkEqual(a,b) {
+return a === b ? true: false;     or just return a === b;
+}
+console.log(checkEqual(1,2));
