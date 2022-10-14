f6a4da0367106cf680c55400e134b48850ebe112
Hanna Ru <112951096+8HannaRu8@users.noreply.github.com>
2022-10-14 12:38:09 +0300

    Update main4.js

--- JavaScript-Algorithms-Data-Structures/BasicJS/main4.js
+++ JavaScript-Algorithms-Data-Structures/BasicJS/main4.js
@@ -25,3 +25,11 @@ function checkEqual(a,b) {
 return a === b ? true: false;     or just return a === b;
 }
 console.log(checkEqual(1,2));
+
+const arr1 = ['JAN', 'FEB', 'MAR'];
+let arr2;
+(function() {
+arr2 = [...arr1];
+arr1[0] = 'potato'
+}) ();
+console.log(arr2);
