2c25dea03c94791c8de2f288d1fa09ea50ac8a3d
Hanna Ru <112951096+8HannaRu8@users.noreply.github.com>
2022-10-28 21:12:02 +0300

    Update main4.js

--- JavaScript-Algorithms-Data-Structures/BasicJS/main4.js
+++ JavaScript-Algorithms-Data-Structures/BasicJS/main4.js
@@ -68,3 +68,22 @@ isCompleted: false
 
 const newToDoList = JSON.stringify(toDoList);
 console.log(newToDoList);
+
+
+
+
+let costumerIsBanned = false;
+let soup = "chicken noodle soup";
+let crackers = true;
+let reply;
+
+if (costumerIsBanned) {
+  reply = `No soup for you!`;
+} else if (soup && crackers) {
+  reply = `Here's your order of ${soup} & crackers`;
+} else if (soup) {
+  reply = `Here's your order of ${soup}`;
+} else {
+  reply = `Sorry, we're out of ${soup}`;
+}
+console.log(reply);
