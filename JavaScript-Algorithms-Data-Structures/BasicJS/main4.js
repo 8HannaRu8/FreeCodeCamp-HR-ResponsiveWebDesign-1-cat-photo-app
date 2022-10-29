f1b1c9e644c7ead8c884546f689cb6cacc4fb493
Hanna Ru <112951096+8HannaRu8@users.noreply.github.com>
2022-10-29 12:25:34 +0300

    Update main4.js

--- JavaScript-Algorithms-Data-Structures/BasicJS/main4.js
+++ JavaScript-Algorithms-Data-Structures/BasicJS/main4.js
@@ -87,3 +87,22 @@ if (costumerIsBanned) {
   reply = `Sorry, we're out of ${soup}`;
 }
 console.log(reply);
+
+
+
+switch (2) {
+  case 1:
+    console.log(1);
+    break;
+
+  case 2:
+    console.log(2);
+    break;
+
+  case 3:
+    console.log(3);
+    break;
+
+  default:
+    console.log(`No match`);
+}
