6ef4046fe84271c17302979e6bd9f730cffadbce
Hanna Ru <112951096+8HannaRu8@users.noreply.github.com>
2022-10-30 10:56:03 +0300

    Update main4.js

--- JavaScript-Algorithms-Data-Structures/BasicJS/main4.js
+++ JavaScript-Algorithms-Data-Structures/BasicJS/main4.js
@@ -106,3 +106,27 @@ switch (2) {
   default:
     console.log(`No match`);
 }
+
+
+  case `rock`:
+    if (computer === `paper`) {
+      console.log(`Computer wins!`);
+    } else {
+      console.log(`PlayerOne wins!`);
+    }
+    break;
+
+  case `paper`:
+    if (computer === `scissors`) {
+      console.log(`Computer wins!`);
+    } else {
+      console.log(`PlayerOne wins!`);
+    }
+    break;
+
+  default:
+    if (computer === `rock`) {
+      console.log(`Computer wins!`);
+    } else {
+      console.log(`PlayerOne wins!`);
+    }
