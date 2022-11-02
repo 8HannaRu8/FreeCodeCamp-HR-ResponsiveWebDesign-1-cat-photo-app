eb8053ac066673b79302db89b59627987bf34dfe
Hanna Ru <112951096+8HannaRu8@users.noreply.github.com>
2022-11-02 19:33:29 +0300

    Delete main3.js

--- JavaScript-Algorithms-Data-Structures/BasicJS/main3.js
+++
@@ -1,9 +0,0 @@
-function randomRange(ourMin, ourMax) {
-return Math.floor(Math.random() * (ourMax - ourMin + 1))+ ourMin;
-}
-randomRange(1,9);
-
-var ourRandom = randomRange(5,15);
-console.log(ourRandom);
-______________________________
-11
